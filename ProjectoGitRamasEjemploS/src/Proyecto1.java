c653341c2badf2f544cf6d7b2d326804724a497a
juandacaor <118214301+juandacaor@users.noreply.github.com>
2022-11-27 21:50:46 +0100

    Update Proyecto1.java
    como al principio

--- ProjectoGitRamasEjemploS/src/Proyecto1.java
+++ ProjectoGitRamasEjemploS/src/Proyecto1.java
@@ -1,9 +1,9 @@
 
 public class Proyecto1 {
 
-	public static void imprimeSaludo() { 
-System.out.println(“Heil”);    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("helloo mundo");
+	}
 }
-
-
 }
