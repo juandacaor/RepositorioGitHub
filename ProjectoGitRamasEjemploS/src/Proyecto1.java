88482f93be18120ac6bb45a8d94b418db8b08dc6
corre <corre@CAR10>
2022-12-19 19:36:54 +0100

    Merge branch 'Repositorio1' of https://github.com/juandacaor/RepositorioGitHub.git into Repositorio1

--- ProjectoGitRamasEjemploS/src/Proyecto1.java
+++ ProjectoGitRamasEjemploS/src/Proyecto1.java
@@ -4,5 +4,6 @@ public class Proyecto1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Saludos mundo");
+		System.out.println("Otro mensajes");
 	}
 }
